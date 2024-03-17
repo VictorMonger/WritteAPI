48f2470ca324ec92cb670a70c67dde4529a6a100
Victor Monger <victormonger2@outlook.com>
2024-03-16 21:55:29 -0300

    routes in mvc class added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ app.use(express.json());
 
 const connection = require("./database/connection");
 
+const UsersModel = require("./models/usersModel");
+const UsersController = require("./controllers/usersController");
+const { UsersRouter } = require("./routes/");
+
+const usersModel = new UsersModel(connection);
+const usersController = new UsersController(usersModel);
+const usersRouter = new UsersRouter(usersController);
+
+app.use("/users", usersRouter.getRoutes());
+
 app.listen(PORT, () => {
   console.log(`Server listening port ${PORT}`);
 });
