b0f488a72b8eea51a65fe679fe9072d17fe272bf
Victor Monger <victormonger2@outlook.com>
2024-03-17 23:05:54 -0300

    auth added

---
+++ src/lib/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+const createUserToken = (user) => {
+  const token = jwt.sign({ user: JSON.stringify(user) }, JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return token;
+};
+
+const verifyToken = (token) => {
+  const payload = jwt.verify(token, JWT_SECRET);
+
+  return payload;
+};
+
+module.exports = {
+  createUserToken,
+  verifyToken,
+};
