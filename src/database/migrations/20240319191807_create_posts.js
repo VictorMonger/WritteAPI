11c40a070edc5dd44d8860cb34b5750f067e50c6
Victor Monger <victormonger2@outlook.com>
2024-03-21 15:27:33 -0300

    migration config

--- src/database/migrations/20240319191807_create_posts.js
+++ src/database/migrations/20240319191807_create_posts.js
@@ -6,10 +6,11 @@ exports.up = function(knex) {
   return knex.schema.createTable("posts", (table) => {
     table.uuid("id").defaultTo(knex.fn.uuid()).primary();
     table.timestamp("created_at").defaultTo(knex.fn.now());
-    table.string("description", [255]).notNullable();
+    table.text("content").notNullable();
+    table.integer("likes").defaultTo(0);
 
-    table.string("userName");
-    table.foreign("userName").references("user.userName").onDelete("CASCADE");
+    table.string("author");
+    table.foreign("author").references("users.userName").onDelete("CASCADE");
   });
 };
 
