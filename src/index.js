341486b53ecd1d96a524c840f60981fd971ceda1
Victor Monger <victormonger2@outlook.com>
2024-03-26 10:57:04 -0300

    new routes added

--- src/index.js
+++ src/index.js
@@ -24,12 +24,24 @@ const PostsController = require("./controllers/postsController");
 const { PostsRouter } = require("./routes/");
 
 const postsModel = new PostsModel(connection);
-const postController = new PostsController(postsModel);
-const postsRouter = new PostsRouter(postController);
+const postsController = new PostsController(postsModel);
+const postsRouter = new PostsRouter(postsController);
+
+const FollowersModel = require("./models/followersModel");
+const FollowersController = require("./controllers/followersController");
+const { FollowersRouter } = require("./routes/");
+
+const followersModel = new FollowersModel(connection);
+const followersController = new FollowersController(followersModel);
+const followersRouter = new FollowersRouter(followersController);
 
 app.use("/users", usersRouter.getRoutes());
 
+app.use("/users/private", verifyToken, usersRouter.getRoutes());
+
 app.use("/posts/", verifyToken, postsRouter.getRoutes())
+
+app.use("/follows/", verifyToken, followersRouter.getRoutes())
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
