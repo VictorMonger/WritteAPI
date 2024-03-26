697c0498ac84240e12a6414b5cf069df9ffa68e3
Victor Monger <victormonger2@outlook.com>
2024-03-26 11:03:50 -0300

    new post routes

--- src/routes/posts/postsRoute.js
+++ src/routes/posts/postsRoute.js
@@ -9,6 +9,22 @@ class PostsRouter {
     this.posts.post("/", (request, response) => {
       this.postsController.createPost(request, response);
     });
+
+    this.posts.delete("/:postId", (request, response) => {
+      this.postsController.deletePost(request, response);
+    });
+
+    this.posts.get("/:postId", (request, response) => {
+      this.postsController.getPostInfo(request, response);
+    });
+
+    this.posts.post("/:postId/like", (request, response) => {
+      this.postsController.likePost(request, response);
+    });
+
+    this.posts.delete("/:postId/unlike", (request, response) => {
+      this.postsController.unlikePost(request, response);
+    });
   }
   
   getRoutes() {
