646ad9f70daaf44522e2e121adc4ffbf314b5d0e
Victor Monger <victormonger2@outlook.com>
2024-03-26 11:03:05 -0300

    followers routes

---
+++ src/routes/followers/followersRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+class FollowersRouter {
+  constructor(followersController) {
+    this.follow = express.Router();
+
+    this.followersController = followersController;
+
+    this.follow.post("/follow", (request, response) => {
+      this.followersController.followUser(request, response);
+    });
+
+    this.follow.delete("/unfollow", (request, response) => {
+      this.followersController.unfollowUser(request, response);
+    });
+  }
+  
+  getRoutes() {
+    return this.follow;
+  }
+}
+
+module.exports = FollowersRouter;
