0e037b71c38ae4da17c728eb83337adfb35a601c
Victor Monger <victormonger2@outlook.com>
2024-03-26 11:02:52 -0300

    new routes connections

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const UsersRouter  = require("./users/usersRoutes");
-const PostsRouter = require("./posts/postsRoute")
+const PostsRouter = require("./posts/postsRoute");
+const FollowersRouter = require("./followers/followersRoutes");
 
 module.exports = {
   UsersRouter,
-  PostsRouter, 
+  PostsRouter,
+  FollowersRouter, 
 };
