272c8c36ddcec0c4379a22f8c99dc45438a6bf5e
Victor Monger <victormonger2@outlook.com>
2024-03-26 11:01:04 -0300

    new routes added

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -1,18 +1,79 @@
 class PostsModel {
   constructor(connection) {
-    this.connection = connection
+    this.connection = connection;
   }
 
-  async createPosts(userName, content) {
+  async createPosts(userId, content) {
     try {
       return await this.connection("posts")
         .insert({
-          userName,
+          userId,
           content,
         })
         .returning("*");
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
+    }
+  }
+
+  async deletePosts(userId, postId) {
+    try {
+      return await this.connection("posts")
+        .where("userId", userId)
+        .andWhere("id", postId)
+        .delete();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getPostInfo(postId) {
+    try {
+      return await this.connection("posts")
+        .select("posts.content", "users.UserName", "posts.likes")
+        .innerJoin("user", "posts.userId", "users.id")
+        .where("posts.id", postId)
+        .first();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async likePost(postId, userId) {
+    try {
+      const alreadyLike = await this.connection("likes")
+        .where("postId", postId)
+        .andWhere("userId", userId)
+        .first();
+
+      if (alreadyLike) {
+        return false;
+      }
+
+      await this.connection.transaction(async (trx) => {
+        await trx("likes").insert({
+          userId,
+          postId,
+        });
+        await trx("posts").where("id", postId).increment("likes", 1);
+
+        return true;
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async unlikePost(postId, userId) {
+    try {
+      await this.connection.transaction(async (trx) => {
+        await trx("likes").where("userId", userId).andWhere("postId", postId).delete();
+        await trx("posts").where("id", postId).decrement("likes", 1);
+      });
+
+      return true;
+    } catch (error) {
+      throw new Error(error);
     }
   }
 }
