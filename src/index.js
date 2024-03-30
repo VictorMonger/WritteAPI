f0060c215298ce03ceaaf9a3bc4367795fe17f4e
Victor Monger <victormonger2@outlook.com>
2024-03-30 15:31:05 -0300

    structural change

--- src/index.js
+++ src/index.js
@@ -15,21 +15,21 @@ const UsersModel = require("./models/usersModel");
 const UsersController = require("./controllers/usersController");
 const { UsersRouter } = require("./routes/");
 
-const usersModel = new UsersModel(connection);
-const usersController = new UsersController(usersModel);
-const usersRouter = new UsersRouter(usersController);
-
 const PostsModel = require("./models/postsModels");
 const PostsController = require("./controllers/postsController");
 const { PostsRouter } = require("./routes/");
 
-const postsModel = new PostsModel(connection);
-const postsController = new PostsController(postsModel);
-const postsRouter = new PostsRouter(postsController);
-
 const FollowersModel = require("./models/followersModel");
 const FollowersController = require("./controllers/followersController");
 const { FollowersRouter } = require("./routes/");
+
+const usersModel = new UsersModel(connection);
+const usersController = new UsersController(usersModel);
+const usersRouter = new UsersRouter(usersController);
+
+const postsModel = new PostsModel(connection);
+const postsController = new PostsController(postsModel);
+const postsRouter = new PostsRouter(postsController);
 
 const followersModel = new FollowersModel(connection);
 const followersController = new FollowersController(followersModel);
