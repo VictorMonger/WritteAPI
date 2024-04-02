096cebb2a8cc9633b0d0447318eca155e0fa86ff
Victor Monger <victormonger2@outlook.com>
2024-04-02 00:52:32 -0300

    new test

--- test/usersModel.test.js
+++ test/usersModel.test.js
@@ -1,23 +1,22 @@
 const UsersModel = require("../src/models/usersModel");
-const connection = require("../src/database/connection");
 
 describe("Users Model", () => {
   describe("validate userNameExists", () => {
     test("should return true if userName already exists", async () => {
-      const usersModel = new UsersModel(connection);
+      const usersModel = new UsersModel();
 
-      const userNameToTest = "userExisting";
+      const existingUserName = "userExisting";
 
       usersModel.connection = jest.fn().mockReturnValue({
         select: jest.fn().mockReturnThis(),
         where: jest.fn().mockReturnThis(),
-        first: jest.fn().mockResolvedValue({ userName: userNameToTest }),
+        first: jest.fn().mockResolvedValue({ userName: existingUserName }),
       });
 
-      const userNameExists = await usersModel.userNameExists(userNameToTest);
+      const userNameExists = await usersModel.userNameExists(existingUserName);
 
       expect(userNameExists).toBeTruthy();
-      expect(userNameExists.userName).toBe(userNameToTest)
+      expect(userNameExists.userName).toBe(existingUserName)
     });
   });
 });
