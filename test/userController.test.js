e38bf83cfa0c512e657b3709321ac9c3ea6458bf
Victor Monger <victormonger2@outlook.com>
2024-04-01 23:31:37 -0300

    test

---
+++ test/userController.test.js
@@ -0,0 +1,13 @@
+const UserController = require("../src/controllers/usersController")
+
+const validateController = new UserController();
+
+describe("UserController", () => {
+  describe("validate SignUp", () => {
+    test("should be able to signUp", () => {
+      const user = {}
+    })
+  })
+}
+)
+
