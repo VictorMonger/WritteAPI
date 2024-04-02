a1626b7ed31e2bf3abfa737bb3f5876eace5b47a
Victor Monger <victormonger2@outlook.com>
2024-04-02 02:47:54 -0300

    userNameExists Unity Test

--- test/usersModel.test.js
+++ test/usersModel.test.js
@@ -1,12 +1,23 @@
-const UsersModel = require ("../src/models/usersModel")
-
-const validateModel = new UsersModel();
+const UsersModel = require("../src/models/usersModel");
+const connection = require("../src/database/connection");
 
 describe("Users Model", () => {
   describe("validate userNameExists", () => {
-    test("should return false to a userName that not exists", () => {
-      const nameToTest = "Color89";
-      const nameToMatch = ""
-    })
-  })
-})
+    test("should return true if userName already exists", async () => {
+      const usersModel = new UsersModel(connection);
+
+      const userNameToTest = "userExisting";
+
+      usersModel.connection = jest.fn().mockReturnValue({
+        select: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        first: jest.fn().mockResolvedValue({ userName: userNameToTest }),
+      });
+
+      const userNameExists = await usersModel.userNameExists(userNameToTest);
+
+      expect(userNameExists).toBeTruthy();
+      expect(userNameExists.userName).toBe(userNameToTest)
+    });
+  });
+});
