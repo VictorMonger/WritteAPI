c0c26c143cd2abe333bed4de819bafb2cd30f1e8
Victor Monger <victormonger2@outlook.com>
2024-04-07 18:37:14 -0300

    try catch added

--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -1,25 +1,29 @@
 const { verifyToken } = require("../lib/auth");
 
 const validateToken = (request, response, next) => {
-  const { authorization } = request.headers;
+  try {
+    const { authorization } = request.headers;
 
-  if (!authorization) {
-    return response.status(400).json({ msg: "Token not provided" });
+    if (!authorization) {
+      return response.status(400).json({ msg: "Token not provided" });
+    }
+
+    const [, token] = authorization.split(" ");
+
+    const payload = verifyToken(token);
+
+    if (!payload) {
+      return response.status(400).json({ msg: "Token not valid" });
+    }
+
+    const { user } = payload;
+
+    request.headers["user"] = user;
+
+    return next();
+  } catch (error) {
+    throw new Error();
   }
-
-  const [, token] = authorization.split(" ");
-
-  const payload = verifyToken(token);
-
-  if (!payload) {
-    return response.status(400).json({ msg: "Token not valid" });
-  }
-
-  const { user } = payload;
-
-  request.headers["user"] = user;
-
-  return next();
 };
 
 module.exports = validateToken;
