0c338aa4d2a0b547060e5a3cce9de316322d503d
Victor Monger <victormonger2@outlook.com>
2024-04-08 18:38:49 -0300

    try catch added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,17 +2,25 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
 const createUserToken = (user) => {
-  const token = jwt.sign({ user: JSON.stringify(user) }, JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  try {
+    const token = jwt.sign({ user: JSON.stringify(user) }, JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
-  return token;
+    return token;
+  } catch (error) {
+    throw new Error();
+  }
 };
 
 const verifyToken = (token) => {
-  const payload = jwt.verify(token, JWT_SECRET);
-  
-  return payload;
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+
+    return payload;
+  } catch (error) {
+    throw new Error();
+  }
 };
 
 module.exports = {
